70d14cf9e1aeb345bd828c6e516df9c09824a7dc
unknown <alnasfire@gmail.com>
2013-03-18 00:09:37 +0430

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="style.css" /> 
+        <title>Tour Today! Hot tours to many resorts!</title>
+    </head>
+    <body class="body">
+        <div class="header">
+        </div>
+        <div class="main">  
+            <div class="leftmenu">
+            </div>
+            <div class="search"> 
+                <form>
+                    <input type="text"/>
+                    <button>search</button>
+                </form>                
+            </div>
+            <div class="content">
+                <table class="tourtable" align="center">
+                   <tr>
+                       <td name="country">country</td>
+                       <td name="city">city</td>
+                       <td name="departure city">departure city</td>
+                       <td name="duration">duration</td>
+                       <td name="firm name">firm name</td>
+                       <td name="hotel">hotel</td>
+                       <td name="phones">phones</td>
+                       <td name="price">price</td>
+                       <td name="date">date</td>
+                   </tr>
+                   <?php
+
+                   ?>
+                </table>            
+            </div>
+            <div class="underground"></div>
+        </div>
+        <?php
+
+        ?>
+    </body>
+</html>
