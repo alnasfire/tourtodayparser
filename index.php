f1693efdc73a5b19c2e031f5f70c9c38ff07032c
unknown <alnasfire@gmail.com>
2013-03-22 00:06:27 +0430

    new style

--- index.php
+++ index.php
@@ -6,32 +6,55 @@
     <body class="body">
         <div class="header">
         </div>
-        <div class="main">  
-            <div class="leftmenu">
-            </div>
-            <div class="search"> 
-                <form>
-                    <input type="text"/>
-                    <button>search</button>
-                </form>                
-            </div>
+        <div class="search"> 
+            <form>
+                <input type="text"/>
+                <button>search</button>
+            </form>                
+        </div>
+        <div class="main">                          
             <div class="content">
-                <table class="tourtable" align="center">
-                   <tr>
-                       <td name="country">country</td>
-                       <td name="city">city</td>
-                       <td name="departure city">departure city</td>
-                       <td name="duration">duration</td>
-                       <td name="firm name">firm name</td>
-                       <td name="hotel">hotel</td>
-                       <td name="phones">phones</td>
-                       <td name="price">price</td>
-                       <td name="date">date</td>
+                <div class="leftmenu">
+                    <table class="menu"> 
+                        <tr>
+                            <td><img src="/tourtoday/images/vk1.png" width="150" height="50"/></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/tourtoday/images/vk2.png" width="150" height="50"/></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/tourtoday/images/vk3.png" width="150" height="50"/></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/tourtoday/images/vk4.png" width="150" height="50"/></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/tourtoday/images/vk5.png" width="150" height="50"/></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/tourtoday/images/vk6.png" width="150" height="50"/></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/tourtoday/images/vk7.png" width="150" height="50"/></td>
+                        </tr>
+                    </table>
+                </div>
+<!--                <table class="tourtable" align="center">
+                    <tr class="tourtabletr">
+                       <td class="tourtabletd" name="country">country</td>
+                       <td class="tourtabletd" name="city">city</td>
+                       <td class="tourtabletd" name="departure city">departure city</td>
+                       <td class="tourtabletd" name="duration">duration</td>
+                       <td class="tourtabletd" name="firm name">firm name</td>
+                       <td class="tourtabletd" name="hotel">hotel</td>
+                       <td class="tourtabletd" name="phones">phones</td>
+                       <td class="tourtabletd" name="price">price</td>
+                       <td class="tourtabletd" name="date">date</td>
                    </tr>
                    <?php
 
                    ?>
-                </table>            
+                </table>            -->
             </div>
             <div class="underground"></div>
         </div>
