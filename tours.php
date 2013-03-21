96e361d2e113bbc0afc92540319cb1a577709d42
unknown <alnasfire@gmail.com>
2013-03-22 00:46:55 +0430

    site map

--- tours.php
+++ tours.php
@@ -6,12 +6,6 @@
     <body class="body">
         <div class="header">
         </div>
-<!--        <div class="search"> 
-            <form>
-                <input type="text"/>
-                <button>search</button>
-            </form>                
-        </div>-->
         <div class="main">                          
             <div class="content">
                 <div class="leftmenu">
@@ -38,28 +32,9 @@
                             <td><a href="about.php"><img src="/tourtoday/images/vk7.png" width="162" height="50"/></a></td>
                         </tr>
                     </table>
-                </div>
-<!--                <table class="tourtable" align="center">
-                    <tr class="tourtabletr">
-                       <td class="tourtabletd" name="country">country</td>
-                       <td class="tourtabletd" name="city">city</td>
-                       <td class="tourtabletd" name="departure city">departure city</td>
-                       <td class="tourtabletd" name="duration">duration</td>
-                       <td class="tourtabletd" name="firm name">firm name</td>
-                       <td class="tourtabletd" name="hotel">hotel</td>
-                       <td class="tourtabletd" name="phones">phones</td>
-                       <td class="tourtabletd" name="price">price</td>
-                       <td class="tourtabletd" name="date">date</td>
-                   </tr>
-                   <?php
-
-                   ?>
-                </table>            -->
+                </div>         
             </div>
             <div class="underground"></div>
         </div>
-        <?php
-
-        ?>
     </body>
 </html>
--- visas.php
+++ visas.php
@@ -6,12 +6,6 @@
     <body class="body">
         <div class="header">
         </div>
-<!--        <div class="search"> 
-            <form>
-                <input type="text"/>
-                <button>search</button>
-            </form>                
-        </div>-->
         <div class="main">                          
             <div class="content">
                 <div class="leftmenu">
@@ -39,27 +33,8 @@
                         </tr>
                     </table>
                 </div>
-<!--                <table class="tourtable" align="center">
-                    <tr class="tourtabletr">
-                       <td class="tourtabletd" name="country">country</td>
-                       <td class="tourtabletd" name="city">city</td>
-                       <td class="tourtabletd" name="departure city">departure city</td>
-                       <td class="tourtabletd" name="duration">duration</td>
-                       <td class="tourtabletd" name="firm name">firm name</td>
-                       <td class="tourtabletd" name="hotel">hotel</td>
-                       <td class="tourtabletd" name="phones">phones</td>
-                       <td class="tourtabletd" name="price">price</td>
-                       <td class="tourtabletd" name="date">date</td>
-                   </tr>
-                   <?php
-
-                   ?>
-                </table>            -->
             </div>
             <div class="underground"></div>
         </div>
-        <?php
-
-        ?>
     </body>
 </html>
